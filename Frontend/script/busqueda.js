a2bea685deea15d39cf49d4bfd27601b5592f3e3
Franco <francoale2010@gmail.com>
2020-03-24 20:27:33 -0300

    Add files via upload

--- Frontend/script/busqueda.js
+++ Frontend/script/busqueda.js
@@ -1,29 +1,29 @@
 function hoverContacto(element) {
-    document.getElementById("mailsito").src = "img/mail%202.png";
+    document.getElementById("icono-contacto").src = "img/mail%202.png";
 }
 
 function hoverCasa(element) {
-    document.getElementById("casita").src = "img/house%202.png";
+    document.getElementById("icono-casa").src = "img/house%202.png";
 }
 
 function hoverLupa(element){
-    document.getElementById("lupita").src = "img/lupa%202.png";
-    document.getElementById("lupita").style.position = "relative";
-    document.getElementById("lupita").style.left = "-10px";   
+    document.getElementById("icono-lupa").src = "img/lupa%202.png";
+    document.getElementById("icono-lupa").style.position = "relative";
+    document.getElementById("icono-lupa").style.left = "-10px";   
 }
 
 function unhoverContacto(element) {
-    document.getElementById("mailsito").src = "img/mail%201.png";
+    document.getElementById("icono-contacto").src = "img/mail%201.png";
 }
 
 function unhoverCasa(element) {
-    document.getElementById("casita").src = "img/house%201.png";
+    document.getElementById("icono-casa").src = "img/house%201.png";
 }
 
 function unhoverLupa(element){
-    document.getElementById("lupita").src = "img/lupa%201.png";
-    document.getElementById("lupita").style.position = "relative";
-    document.getElementById("lupita").style.left = "-10px";
+    document.getElementById("icono-lupa").src = "img/lupa%201.png";
+    document.getElementById("icono-lupa").style.position = "relative";
+    document.getElementById("icono-lupa").style.left = "-10px";
 }
 
 $(function () {
